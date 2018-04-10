d8ce6ec0ac355925407b12d209031939b1b65da7
ZhuGuohua <zgh_email@163.com>
2018-04-10 18:09:32 +0800

    提交原始代码

---
+++ Module/inc/DumbExample.h
@@ -0,0 +1,15 @@
+/*
+ * DumbExample.h
+ *
+ *  Created on: 2018Äê3ÔÂ8ÈÕ
+ *      Author: zhuguohua
+ */
+
+#ifndef DUMBEXAMPLE_H_
+#define DUMBEXAMPLE_H_
+
+#include <stdint.h>
+
+int AverageThreeBytes(int a, int b, int c);
+
+#endif /* DUMBEXAMPLE_H_ */
---
+++ Module/src/DumbExample.c
@@ -0,0 +1,13 @@
+/*
+ * DumbExample.c
+ *
+ *  Created on: 2018Äê3ÔÂ8ÈÕ
+ *      Author: zhuguohua
+ */
+#include "DumbExample.h"
+
+int AverageThreeBytes(int a, int b, int c)
+{
+    return (int)(((int32_t)a + (int32_t)b + (int32_t)c) / 3);
+}
+
